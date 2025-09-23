2e93227dd7b97dc156d47ca329b1045e08d0cf2c
Christian <cxbeale@gmail.com>
2025-09-23 19:05:07 +0100

    add further models and update

--- config/settings.py
+++ config/settings.py
@@ -26,6 +26,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-+!vcg&bf+01ft3q2i&sudcxz*9r(q0^%y+4j+c_w94=s=r-a+5'
 
+SECRET_KEY = os.environ.get("SECRET_KEY")
+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
